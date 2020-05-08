3984ca5b559f0281e551ae99d187f6ff182d6457
inesrichard <ines.richard@hetic.net>
2020-05-09 01:36:24 +0200

    Add animation and song keydown

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-const themeDisplay = document.querySelector('.theme_display')
+// SET THE DARK MODE
+
+const themeDisplay = document.querySelector('.feat1')
 
 function themeDisplayChange()
 {
     let themeContent = themeDisplay.textContent
-    if(themeContent == 'Light')
+    if(themeContent == 'Dark')
     {
         document.documentElement.setAttribute('data-theme', 'dark');
-        themeDisplay.textContent = 'Dark'
+        themeDisplay.textContent = 'Light'
     
     }
-    else if(themeContent == 'Dark')
+    else if(themeContent == 'Light')
     {
         document.documentElement.setAttribute('data-theme', 'default')
-        themeDisplay.textContent = 'Light'
+        themeDisplay.textContent = 'Dark'
     }
 }
 
 themeDisplay.addEventListener('click', themeDisplayChange)
+
+
+// PLAY THE AUDIO ON KEYDOWN
+
+window.addEventListener('keydown', function(e)
+{
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const box = document.querySelector(`.box[data-key="${e.keyCode}"]`)
+    if(!audio) return
+    audio.play()
+    audio.currentTime = 0
+    box.classList.add('anime') // ADD THE ANIMATION WHEN THE AUDIO IS PLAYED
+})
+
+function removeTransition(e) 
+{
+    if (e.propertyName !== 'transform') return
+    this.classList.remove('anime')
+    console.log(e.propertyName)
+}
+
+const keys = document.querySelectorAll('.box')
+keys.forEach(box => box.addEventListener('transitionend', removeTransition)) // REMOVE THE ANIMATION WHEN THE AUDIO IS OVER
+
+  
+// PLAY THE AUDIO ON CLICK
+
