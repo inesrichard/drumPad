168295cf2dfd214f0ea062494dee8239f045d494
inesrichard <ines.richard@hetic.net>
2020-05-11 15:16:03 +0200

    Add "Play the mood song if selected "

--- scripts/drumpad.js
+++ scripts/drumpad.js
@@ -1,3 +1,49 @@
+// --- SAMPLES --- //
+
+// PLAY THE SAMPLE AUDIO ON KEYDOWN
+
+window.addEventListener('keydown', function(e)
+{
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const box = document.querySelector(`.box[data-key="${e.keyCode}"]`)
+    if(!audio) return
+    audio.currentTime = 0
+    audio.play()
+    box.classList.add('animation') // ADD THE ANIMATION WHEN THE AUDIO IS PLAYED
+})
+
+function removeTransition(e) 
+{
+    if (e.propertyName !== 'transform') return
+    this.classList.remove('animation')
+    console.log(e.propertyName)
+}
+
+const keys = document.querySelectorAll('.box')
+keys.forEach(box => box.addEventListener('transitionend', removeTransition)) // REMOVE THE ANIMATION WHEN THE AUDIO IS OVER
+
+
+
+// PLAY THE SAMPLE AUDIO ON CLICK
+
+let box 
+
+keys.forEach((box) => {
+    box.addEventListener('click', (e) => { 
+        console.log(box.getAttribute('data-key'))
+        const audio = document.querySelector(`audio[data-key="${box.getAttribute('data-key')}"]`)
+        console.log(audio) 
+        if(!audio) return
+        audio.currentTime = 0
+        audio.play()
+        box.classList.add('animation') // ADD THE ANIMATION WHEN THE AUDIO IS PLAYED   
+        })
+})
+
+
+
+// --- FEATURES --- //
+
 // SET THE DARK MODE
 
 const themeDisplay = document.querySelector('.feat1')
@@ -22,47 +68,41 @@ function themeDisplayChange()
 themeDisplay.addEventListener('click', themeDisplayChange)
 
 
-// PLAY THE AUDIO ON KEYDOWN
 
-window.addEventListener('keydown', function(e)
-{
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const box = document.querySelector(`.box[data-key="${e.keyCode}"]`)
-    if(!audio) return
-    audio.currentTime = 0
-    audio.play()
-    box.classList.add('animation') // ADD THE ANIMATION WHEN THE AUDIO IS PLAYED
-})
+// PLAY THE MOOD AUDIO ON CLICK WHEN IT IS SELECTED
 
-function removeTransition(e) 
-{
-    if (e.propertyName !== 'transform') return
-    this.classList.remove('animation')
-    console.log(e.propertyName)
-}
+let bar = document.querySelector('audio[data-key="37"]')
+let hall = document.querySelector('audio[data-key="38"]')
+let garden = document.querySelector('audio[data-key="39"]')
+let sea = document.querySelector('audio[data-key="40"]')
+const feat3 = document.querySelector('.feat3')
+let selected 
 
-const keys = document.querySelectorAll('.box')
-keys.forEach(box => box.addEventListener('transitionend', removeTransition)) // REMOVE THE ANIMATION WHEN THE AUDIO IS OVER
+console.log(bar)
 
 
-// PLAY THE AUDIO ON CLICK
-
-let box 
-
-keys.forEach((box) => {
-    box.addEventListener('click', (e) => { 
-        console.log(box.getAttribute('data-key'))
-        const audio = document.querySelector(`audio[data-key="${box.getAttribute('data-key')}"]`)
+feat3.addEventListener('input', (e) => { 
+    bar.pause() //STOP THE AUDIO "Bar" CURRENTLY PLAYED IF ANOTHER IS SELECTED
+    hall.pause() //STOP THE AUDIO "Hall" CURRENTLY PLAYED IF ANOTHER IS SELECTED
+    garden.pause() //STOP THE AUDIO "Garden" CURRENTLY PLAYED IF ANOTHER IS SELECTED
+    sea.pause() //STOP THE AUDIO "Sea" CURRENTLY PLAYED IF ANOTHER IS SELECTED
+    selected = feat3.options[feat3.selectedIndex].textContent
+    {
+        const audio = document.querySelector(`audio[data-key="${feat3.options[feat3.selectedIndex].getAttribute('data-key')}"]`)
         console.log(audio) 
         if(!audio) return
         audio.currentTime = 0
         audio.play()
-        box.classList.add('animation') // ADD THE ANIMATION WHEN THE AUDIO IS PLAYED   
-        })
+    }
 })
 
 
-// VOLUME BAR
+    // if(selected === bar)
+
+
+
+
+// VOLUME BAR - LINK THE VOLUME TO THE RANGE SLIDER AND DISPLAY THE VALUE
 
 const volumeContainer = document.querySelector('.volume_container')
 const slider = volumeContainer.querySelector('.volume')
